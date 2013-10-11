ae46c02edd0af12542991dae02ec1a7c8ea52d89
Enrico <Enrico.Mingo@iit.it>
2013-10-11 20:28:14 +0200

    Added possibility to send position command to the robot. Needs to be cleaned (Thanks to Alessio Rocchi's Help!)

--- src/gazebo_world.cpp
+++ src/gazebo_world.cpp
@@ -1,4 +1,4 @@
 #include <gazebo_world.h>
 
-gazebo::physics::WorldPtr gazebo_world::worldPtr;
 gazebo::physics::ModelPtr gazebo_world::modelPtr;
+yarp::sig::Vector gazebo_world::ref_pos;
--- src/yarp_gazebo.cc
+++ src/yarp_gazebo.cc
@@ -13,15 +13,20 @@
 #include <fakebot.h>
 
 #define ms(X) (X * 1000.0)
+#define toRad(X) (X*M_PI/180.0)
 #define ROBOT_NAME "COMAN"
 
 namespace gazebo
 {
-    class yarp_gazebo : public WorldPlugin
+    class yarp_gazebo : public ModelPlugin
     {
     public:
-        yarp_gazebo(): WorldPlugin()
-          ,_yarp()
+        yarp_gazebo() : _yarp()
+        {
+
+        }
+
+        void Init()
         {
             std::cout<<"*** GAZEBO 2 YARP ***"<<std::endl;
             if (!_yarp.checkNetwork())
@@ -32,22 +37,18 @@ namespace gazebo
 
          ~yarp_gazebo()
         {
-            std::cout<<"Goodbye!"<<std::endl;
             _driver.close();
+            std::cout<<"Goodbye!"<<std::endl;
         }
 
-        void Load(physics::WorldPtr _parent, sdf::ElementPtr _sdf)
+        void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
         {
-            this->_world = _parent;
+            this->_robot = _parent;
 
-            gazebo_world::setWorld(_world);
-
-            _dT = ms(_world->GetPhysicsEngine()->GetUpdatePeriod());
+            _dT = ms(this->_robot->GetWorld()->GetPhysicsEngine()->GetUpdatePeriod());
             std::cout<<"Simulation Time Step: "<<_dT<<" [ms]"<<std::endl;
 
-
-            this->_robot = _world->GetModel(ROBOT_NAME);
-            gazebo_world::setModel(_robot);
+            gazebo_world::setModel(this->_robot);
             std::cout<<"Robot Name: "<<_robot->GetName()<<std::endl;
             std::cout<<"# Joints: "<<_robot->GetJoints().size()<<std::endl;
             std::cout<<"# Links: "<<_robot->GetLinks().size()<<std::endl;
@@ -56,7 +57,7 @@ namespace gazebo
                                               ("fakebot", "controlboard", "fakebot"));
             _parameters.put("device", "controlboard");
             _parameters.put("subdevice", "fakebot");
-            _parameters.put("name", "/fakebot");
+            _parameters.put("name", "/fakebot/head");
             _parameters.put("dT", _dT);
 
 
@@ -67,11 +68,9 @@ namespace gazebo
             printf("Device initialized correctly, now sitting and waiting\n");
 
             std::cout<<"LOADED GAZEBO 2 YARP PLUGIN!"<<std::endl;
-
         }
 
     private:
-        physics::WorldPtr _world;
         /**
           * Simulation Time Step in ms
           */
@@ -80,7 +79,8 @@ namespace gazebo
         yarp::os::Network _yarp;
         yarp::dev::PolyDriver _driver;
         yarp::os::Property _parameters;
+        event::ConnectionPtr updateConnection; // Pointer to the update event connection
     };
 
-    GZ_REGISTER_WORLD_PLUGIN(yarp_gazebo)
+    GZ_REGISTER_MODEL_PLUGIN(yarp_gazebo)
 }

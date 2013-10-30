f8255488a99875d5b77cde90a903aee3f18ff029
Enrico <Enrico.Mingo@iit.it>
2013-10-30 15:27:16 +0100

    IMU plugin

---
+++ include/fakebotIMUplugin.hh
@@ -0,0 +1,32 @@
+#ifndef _FAKEBOT_IMU_PLUGIN_HH_
+#define _FAKEBOT_IMU_PLUGIN_HH_
+
+#include <gazebo/gazebo.hh>
+#include <gazebo/sensors/ImuSensor.hh>
+#include <yarp/os/Network.h>
+#include <yarp/dev/PolyDriver.h>
+
+namespace gazebo
+{
+    class fakebotIMUplugin : public SensorPlugin
+    {
+    public:
+        fakebotIMUplugin();
+        virtual ~fakebotIMUplugin();
+
+        virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
+
+    private:
+        sensors::ImuSensorPtr parentSensor;
+        event::ConnectionPtr updateConnection;
+        yarp::os::Network _yarp;
+        yarp::os::Port _p;
+        yarp::os::Bottle _bot;
+
+        virtual void Init();
+        virtual void OnUpdate();
+
+    };
+}
+
+#endif
---
+++ src/fakebotIMUplugin.cc
@@ -0,0 +1,95 @@
+#include "fakebotIMUplugin.hh"
+#include <yarp/dev/ServerInertial.h>
+#include <yarp/dev/PolyDriver.h>
+
+using namespace gazebo;
+GZ_REGISTER_SENSOR_PLUGIN(fakebotIMUplugin)
+
+fakebotIMUplugin::fakebotIMUplugin() : SensorPlugin(),
+    _yarp(),
+    _p(),
+    _bot()
+{
+    _p.open("/coman/inertial:o");
+}
+
+void fakebotIMUplugin::Init()
+{
+    std::cout<<"*** fakebotIMUplugin started ***"<<std::endl;
+    if (!_yarp.checkNetwork())
+        std::cout<<"Sorry YARP network does not seem to be available, is the yarp server available?"<<std::endl;
+    else
+        std::cout<<"YARP Server found!"<<std::endl;
+}
+
+fakebotIMUplugin::~fakebotIMUplugin()
+{
+    std::cout<<"IMU says: Goodbye!"<<std::endl;
+}
+
+void fakebotIMUplugin::Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
+{
+    std::cout<<"IMU Sensor ID:"<<_sensor->GetId()<<std::endl;
+    this->parentSensor =
+        boost::shared_dynamic_cast<sensors::ImuSensor>(_sensor);
+
+    if (!this->parentSensor)
+    {
+        gzerr << "fakebotPlugin requires a IMUSensor.\n";
+        return;
+    }
+
+    this->updateConnection = this->parentSensor->ConnectUpdated(
+          boost::bind(&fakebotIMUplugin::OnUpdate, this));
+
+    this->parentSensor->SetActive(true);
+}
+
+/**
+ *
+ * Export an inertial sensor.
+ * The network interface is a single Port.
+ * We will stream bottles with 12 floats:
+ * 0  1   2  = Euler orientation data (Kalman filter processed)
+ * 3  4   5  = Calibrated 3-axis (X, Y, Z) acceleration data
+ * 6  7   8  = Calibrated 3-axis (X, Y, Z) gyroscope data
+ * 9 10 11   = Calibrated 3-axis (X, Y, Z) magnetometer data
+ *
+ */
+void fakebotIMUplugin::OnUpdate()
+{
+    msgs::IMU imu_data;
+    imu_data = this->parentSensor->GetImuMessage();
+
+    _bot.addDouble(imu_data.mutable_orientation()->x());
+    _bot.addDouble(imu_data.mutable_orientation()->y());
+    _bot.addDouble(imu_data.mutable_orientation()->z());
+//    std::cout<<"Orientation: [ "<<imu_data.mutable_orientation()->x()<<" "<<
+//               imu_data.mutable_orientation()->y()<<" "<<
+//               imu_data.mutable_orientation()->z()<<std::endl;
+
+    _bot.addDouble(imu_data.mutable_linear_acceleration()->x());
+    _bot.addDouble(imu_data.mutable_linear_acceleration()->y());
+    _bot.addDouble(imu_data.mutable_linear_acceleration()->z());
+//    std::cout<<"Linear Acceleration: [ "<<imu_data.mutable_linear_acceleration()->x()<<" "<<
+//               imu_data.mutable_linear_acceleration()->y()<<" "<<
+//               imu_data.mutable_linear_acceleration()->z()<<std::endl;
+
+    _bot.addDouble(imu_data.mutable_angular_velocity()->x());
+    _bot.addDouble(imu_data.mutable_angular_velocity()->y());
+    _bot.addDouble(imu_data.mutable_angular_velocity()->z());
+//    std::cout<<"Angular Velocity: [ "<<imu_data.mutable_linear_acceleration()->x()<<" "<<
+//               imu_data.mutable_linear_acceleration()->y()<<" "<<
+//               imu_data.mutable_linear_acceleration()->z()<<std::endl;
+
+    _bot.addDouble(imu_data.mutable_orientation()->x());
+    _bot.addDouble(imu_data.mutable_orientation()->y());
+    _bot.addDouble(imu_data.mutable_orientation()->z());
+
+    yarp::os::Stamp ts;
+    ts = yarp::os::Stamp(0, imu_data.mutable_stamp()->sec());
+    _p.setEnvelope(ts);
+
+    _p.write(_bot);
+    _bot.clear();
+}

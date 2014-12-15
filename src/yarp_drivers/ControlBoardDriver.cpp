5c2173f0362a1c598ba958baddaefe032955d834
mikearagao <miguelaragao91@gmail.com>
2014-12-15 17:01:39 +0000

    back to update info time step

--- src/yarp_drivers/ControlBoardDriver.cpp
+++ src/yarp_drivers/ControlBoardDriver.cpp
@@ -37,7 +37,7 @@ bool GazeboYarpControlBoardDriver::gazebo_init()
     std::cout<<"# Joints: "<<m_robot->GetJoints().size() <<std::endl;
     std::cout<<"# Links: "<<m_robot->GetLinks().size() <<std::endl;
 
-    this->m_robotRefreshPeriod = this->m_robot->GetWorld()->GetPhysicsEngine()->GetUpdatePeriod() * 1000.0;
+    this->m_robotRefreshPeriod = 0.01 * 1000.0;
     if (!setJointNames()) return false;
 
     m_numberOfJoints = m_jointNames.size();
@@ -98,7 +98,7 @@ bool GazeboYarpControlBoardDriver::gazebo_init()
 
     m_gazeboNode = gazebo::transport::NodePtr(new gazebo::transport::Node);
     m_gazeboNode->Init(this->m_robot->GetWorld()->GetName());
-    // m_jointCommandPublisher = m_gazeboNode->Advertise<gazebo::msgs::JointCmd>(std::string("~/") + this->m_robot->GetName() + "/joint_cmd");
+    m_jointCommandPublisher = m_gazeboNode->Advertise<gazebo::msgs::JointCmd>(std::string("~/") + this->m_robot->GetName() + "/joint_cmd");
 
     _T_controller = 1;
 
@@ -151,11 +151,12 @@ void GazeboYarpControlBoardDriver::onUpdate(const gazebo::common::UpdateInfo& _i
 
     if (!started) {//This is a simple way to start with the robot in standing position
         started = true;
+        std::cout<<"\n\nRefresh period: "<<m_robot->GetWorld()->GetPhysicsEngine()->GetUpdatePeriod() <<"\n\n"<<std::endl;
         for (unsigned int j = 0; j < m_numberOfJoints; ++j) {
             sendPositionToGazebo (j, m_positions[j]);
-            double diff = m_jointPointers[j]->GetAngle(0).Radian() - GazeboYarpPlugins::convertDegreesToRadians(m_referencePositions[j]);
-            double cmd = m_gazeboPositionPIDs[j].Update(diff, 0.01);
-            m_jointPointers[j]->SetForce(0, cmd);
+            //double diff = m_jointPointers[j]->GetAngle(0).Radian() - GazeboYarpPlugins::convertDegreesToRadians(m_referencePositions[j]);
+            //double cmd = m_gazeboPositionPIDs[j].Update(diff, 0.01);
+            //m_jointPointers[j]->SetForce(0, cmd);
             //std::cout << "Joint number: " << j << " P: " << m_gazeboPositionPIDs[m_jointNames[j]].GetPGain() << " I: " << m_gazeboPositionPIDs[m_jointNames[j]].GetIGain() << " D: " << m_gazeboPositionPIDs[m_jointNames[j]].GetDGain() << std::endl;
         }
         //m_jointController = m_robot->GetJointController();
@@ -179,9 +180,11 @@ void GazeboYarpControlBoardDriver::onUpdate(const gazebo::common::UpdateInfo& _i
 
     // Updating timestamp
     //gazebo::common::Time currTime = gazebo::common::Time(_info.simTime.Double());
-    gazebo::common::Time currTime = m_robot->GetWorld()->GetSimTime();
+    /*gazebo::common::Time currTime = m_robot->GetWorld()->GetSimTime();
     gazebo::common::Time stepTime = currTime - previousGazeboSimTime;
-    previousGazeboSimTime = currTime;
+    previousGazeboSimTime = currTime;*/
+    gazebo::common::Time currTime = gazebo::common::Time(_info.simTime.Double());
+    gazebo::common::Time stepTime = currTime - m_lastTimestamp.getTime();
     
     m_lastTimestamp.update(_info.simTime.Double());
 
@@ -191,16 +194,16 @@ void GazeboYarpControlBoardDriver::onUpdate(const gazebo::common::UpdateInfo& _i
         //set pos joint value, set m_referenceVelocities joint value
         if ((m_controlMode[j] == VOCAB_CM_POSITION || m_controlMode[j] == VOCAB_CM_POSITION_DIRECT)
             && (m_interactionMode[j] == VOCAB_IM_STIFF)) {
-            //if (stepTime  > 0.0) {
+            if (stepTime  > 0.0) {
                 if (m_clock % _T_controller == 0) {
                     if (m_controlMode[j] == VOCAB_CM_POSITION) {
                         computeTrajectory(j);
-                        std::cout<<"\n\nhey\n\n"<<std::endl;
+                        //std::cout<<"\n\nhey\n\n"<<std::endl;
                     }
                     // sendPositionToGazebo(j, m_referencePositions[j]);
                     //if (stepTime > 0.0) {
                         double diff = m_jointPointers[j]->GetAngle(0).Radian() - GazeboYarpPlugins::convertDegreesToRadians(m_referencePositions[j]);
-                        std::cout<<"\n\ndiff: "<< diff <<"\n\n"<<std::endl;
+                        //std::cout<<"\n\ndiff: "<< diff <<"\n\n"<<std::endl;
                         //if (j == 5)
                         //std::cout<<"\n\nstep time: "<<stepTime.Double()<<"\n\n"<<std::endl;
                         //if (diff > GazeboYarpPlugins::convertDegreesToRadians(1.0)) {
@@ -214,7 +217,7 @@ void GazeboYarpControlBoardDriver::onUpdate(const gazebo::common::UpdateInfo& _i
                         //}
                     //}
                 }
-            //}
+            }
         } else if ((m_controlMode[j] == VOCAB_CM_VELOCITY) && (m_interactionMode[j] == VOCAB_IM_STIFF)) {//set vmo joint value
             if (m_clock % _T_controller == 0) {
                 sendVelocityToGazebo(j, m_referenceVelocities[j]);
@@ -323,6 +326,11 @@ void GazeboYarpControlBoardDriver::setPIDsForGroup(std::string pidGroupName,
             pidValue.maxInt = pid.get(4).asDouble();
             pidValue.maxOut = pid.get(5).asDouble();
 
+            pidController->SetIMin(-1);
+            pidController->SetIMax(1);
+            pidController->SetCmdMin(-1000);
+            pidController->SetCmdMax(1000);
+
             pidsGazebo.push_back(*pidController);
             pids.push_back(pidValue);
             std::cout<<"  P: "<<pidValue.p<<" I: "<<pidValue.i<<" D: "<<pidValue.d<<" maxInt: "<<pidValue.maxInt<<" maxOut: "<<pidValue.maxOut<<std::endl;
@@ -418,8 +426,8 @@ bool GazeboYarpControlBoardDriver::sendPositionToGazebo(int j,double ref)
 {
     gazebo::msgs::JointCmd j_cmd;
     prepareJointMsg(j_cmd,j,ref);
-    // m_jointCommandPublisher->WaitForConnection();
-    // m_jointCommandPublisher->Publish(j_cmd);
+    m_jointCommandPublisher->WaitForConnection();
+    m_jointCommandPublisher->Publish(j_cmd);
     return true;
 }
 
@@ -458,8 +466,8 @@ bool GazeboYarpControlBoardDriver::sendVelocityToGazebo(int j,double ref) //NOT 
      *      when you switch to velocity mode, to make sure the PIDs get reset */
     gazebo::msgs::JointCmd j_cmd;
     prepareJointVelocityMsg(j_cmd,j,ref);
-    // m_jointCommandPublisher->WaitForConnection();
-    // m_jointCommandPublisher->Publish(j_cmd);
+    m_jointCommandPublisher->WaitForConnection();
+    m_jointCommandPublisher->Publish(j_cmd);
     /**/
     return true;
 }
@@ -498,8 +506,8 @@ bool GazeboYarpControlBoardDriver::sendTorqueToGazebo(const int j,const double r
 {
     gazebo::msgs::JointCmd j_cmd;
     prepareJointTorqueMsg(j_cmd,j,ref);
-    // m_jointCommandPublisher->WaitForConnection();
-    // m_jointCommandPublisher->Publish(j_cmd);
+    m_jointCommandPublisher->WaitForConnection();
+    m_jointCommandPublisher->Publish(j_cmd);
     return true;
 }
 

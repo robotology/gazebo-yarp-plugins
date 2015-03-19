5caeb3f917191ef89b06b88bccabe655f4d40474
Alberto Cardellino <alberto.cardellino@iit.it>
2015-03-19 14:54:09 +0100

    Check ROS group param from config file and pass it to the wrapper

--- src/gazebo_plugins/ControlBoard.cc
+++ src/gazebo_plugins/ControlBoard.cc
@@ -78,6 +78,13 @@ GZ_REGISTER_MODEL_PLUGIN(GazeboYarpControlBoard)
                     printf("GazeboYarpControlBoard::Load  Error: [WRAPPER] group not found in config file\n");
                     return;
                 }
+                std::cout << "\n\nm_parameters.findGroup(ROS): \n\t" << m_parameters.findGroup("ROS").toString() << std::endl;
+                if(m_parameters.check("ROS"))
+                {
+                    yarp::os::ConstString ROS;
+                    ROS = yarp::os::ConstString ("(") + m_parameters.findGroup("ROS").toString() + yarp::os::ConstString (")");
+                    wrapper_group.append(yarp::os::Bottle(ROS));
+                }
                 configuration_loaded = true;
             }
         }

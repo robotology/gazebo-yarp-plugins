59e92fce4770158088623c8cc3934165693b6cca
Silvio Traversaro <silvio.traversaro@iit.it>
2015-05-05 11:29:19 +0200

    Add support for Gazebo 6

--- plugins/controlboard/src/ControlBoardDriver.cpp
+++ plugins/controlboard/src/ControlBoardDriver.cpp
@@ -117,9 +117,13 @@ bool GazeboYarpControlBoardDriver::gazebo_init()
         std::cout<<"INITIAL CONFIGURATION IS: "<<initial_config.toString()<<std::endl;
 
         for (unsigned int i = 0; i < m_numberOfJoints; ++i) {
+#if GAZEBO_MAJOR_VERSION >= 4
+            m_jointPointers[i]->SetPosition(0,initial_config[i]);
+#else
             gazebo::math::Angle a;
             a.SetFromRadian(initial_config[i]);
             m_jointPointers[i]->SetAngle(0,a);
+#endif
         }
     }
     return true;

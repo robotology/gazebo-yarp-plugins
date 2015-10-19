7f5bfceddbfced0853fadd7567fed1137e7cc9fc
Silvio Traversaro <silvio.traversaro@iit.it>
2015-10-19 23:13:38 +0200

    Provide C++98 compatible splitString

--- libraries/singleton/src/ConfHelpers.cc
+++ libraries/singleton/src/ConfHelpers.cc
@@ -22,18 +22,25 @@ using namespace gazebo;
 namespace GazeboYarpPlugins {
 
 /**
- * Split a string in a vector of string given a delimiter
+ * Split a string in a vector of string given a (single char) delimiter
  */
-std::vector<std::string> splitString(const std::string &s, const std::string &delims)
+std::vector<std::string> splitString(const std::string &s, const std::string &delim)
 {
-    std::vector<std::string> result;
-    std::string::size_type pos = 0;
-    while (std::string::npos != (pos = s.find_first_not_of(delims, pos))) {
-        auto pos2 = s.find_first_of(delims, pos);
-        result.emplace_back(s.substr(pos, std::string::npos == pos2 ? pos2 : pos2 - pos));
-        pos = pos2;
+    std::vector<std::string> retVec;
+
+    size_t start = s.find_first_not_of(delim), end=start;
+
+    while (start != std::string::npos)
+    {
+        // Find next occurence of delimiter
+        end = s.find(delim, start);
+        // Push back the token found into vector
+        retVec.push_back(s.substr(start, end-start));
+        // Skip all occurences of the delimiter to find new start
+        start = s.find_first_not_of(delim, end);
     }
-    return result;
+
+    return retVec;
 }
 
 bool addGazeboEnviromentalVariablesModel(gazebo::physics::ModelPtr _parent,
@@ -82,7 +89,7 @@ bool addGazeboEnviromentalVariablesSensor(gazebo::sensors::SensorPtr _sensor,
 
     // Extract the robot name from the sensor scoped name
     std::string scopedSensorName = _sensor->GetScopedName();
-    std::vector<std::string> explodedScopedSensorName = splitString(scopedSensorName,"::");
+    std::vector<std::string> explodedScopedSensorName = splitString(scopedSensorName,":");
 
     // The vector should be at least of 3 elements because
     // scopedSensorName should be something similar to
@@ -96,6 +103,7 @@ bool addGazeboEnviromentalVariablesSensor(gazebo::sensors::SensorPtr _sensor,
 
     std::string gazeboYarpPluginsRobotName = explodedScopedSensorName[explodedScopedSensorName.size()-3];
     plugin_parameters.put("gazeboYarpPluginsRobotName",gazeboYarpPluginsRobotName.c_str());
+
     return true;
 }
 

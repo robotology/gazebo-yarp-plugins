d26164e3f7c6850217af51cb3f710fd090318a55
randaz81 <marco.randazzo@iit.it>
2016-01-08 04:58:36 -0800

    SetPID sends the message to Gazebo core immediatly

--- plugins/controlboard/src/ControlBoardDriverPidControl.cpp
+++ plugins/controlboard/src/ControlBoardDriverPidControl.cpp
@@ -8,6 +8,8 @@
 
 #include "ControlBoardDriver.h"
 #include <GazeboYarpPlugins/common.h>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/transport/transport.hh>
 
 namespace yarp {
     namespace dev {
@@ -21,6 +23,19 @@ namespace yarp {
             // The output limits are only related to the output, so they don't need to be converted
             m_positionPIDs[j].maxInt = pid.max_int;
             m_positionPIDs[j].maxOut = pid.max_output;
+
+            gazebo::msgs::JointCmd j_cmd;
+            j_cmd.set_name(m_jointPointers[j]->GetScopedName());
+            j_cmd.mutable_position()->clear_target();       // This function marks the target field as unused (and it sets internal value to zero)
+            j_cmd.mutable_position()->set_p_gain(m_positionPIDs[j].p);
+            j_cmd.mutable_position()->set_i_gain(m_positionPIDs[j].i );
+            j_cmd.mutable_position()->set_d_gain( m_positionPIDs[j].d);
+            j_cmd.mutable_position()->set_i_max(m_positionPIDs[j].maxInt);
+            j_cmd.mutable_position()->set_i_min(-m_positionPIDs[j].maxInt);
+            j_cmd.mutable_position()->set_limit(m_positionPIDs[j].maxOut);
+
+            m_jointCommandPublisher->WaitForConnection();
+            m_jointCommandPublisher->Publish(j_cmd);
         }
 
         bool GazeboYarpControlBoardDriver::setPids (const Pid *pids)

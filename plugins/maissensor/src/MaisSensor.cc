d501ea1930e99b8a5fc17e88ae56b09ce1b8e994
icub <alberto.cardellino@iit.it>
2016-07-15 13:57:35 +0200

    Propagate ROS parameter to yarp wrapper

--- plugins/maissensor/src/MaisSensor.cc
+++ plugins/maissensor/src/MaisSensor.cc
@@ -86,6 +86,13 @@ void GazeboYarpMaisSensor::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
                 yError("GazeboYarpControlBoard : [WRAPPER] group not found in config file\n");
                 return;
             }
+
+            if(m_parameters.check("ROS"))
+            {
+                yarp::os::ConstString ROS;
+                ROS = yarp::os::ConstString ("(") + m_parameters.findGroup("ROS").toString() + yarp::os::ConstString (")");
+                wrapper_group.append(yarp::os::Bottle(ROS));
+            }
                 
             configuration_loaded = true;
         }

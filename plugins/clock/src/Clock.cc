f6ea9241efff2c05805f8dbf84ab41fee752df1e
ltricerri <triccyxgmail.com>
2018-02-13 16:23:39 +0100

    bugfix reset sim but no time

--- plugins/clock/src/Clock.cc
+++ plugins/clock/src/Clock.cc
@@ -179,7 +179,7 @@ namespace gazebo
 
     void GazeboYarpClock::reset()
     {
-        m_world->Reset();
+        m_world->ResetEntities(gazebo::physics::Base::MODEL);
     }
 
     common::Time GazeboYarpClock::getSimulationTime()

bb47c42f0c798873ef6a74c008346211e998e7ad
Ugo Pattacini <ugo.pattacini@iit.it>
2019-09-20 21:38:31 +0200

    Update CameraDriver.h
    fix #441

--- plugins/camera/include/yarp/dev/CameraDriver.h
+++ plugins/camera/include/yarp/dev/CameraDriver.h
@@ -10,7 +10,7 @@
 #include <yarp/dev/DeviceDriver.h>
 #include <yarp/dev/FrameGrabberInterfaces.h>
 #include <yarp/os/Stamp.h>
-#include <yarp/dev/PreciselyTimed.h>
+#include <yarp/dev/IPreciselyTimed.h>
 #include <yarp/os/Semaphore.h>
 #include <yarp/os/Time.h>
 

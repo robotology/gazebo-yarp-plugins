828884013c89d8e6566a5868783a6f173bf0d9ad
morpheus1820 <rosa.stefano@gmail.com>
2022-10-27 13:11:29 +0000

    corrected paths

--- plugins/pedestriansfm/scripts/start_sim.sh
+++ plugins/pedestriansfm/scripts/start_sim.sh
@@ -1,6 +1,6 @@
 yarpserver &
 sleep 1;
-ros2 launch gazebo_ros gazebo.launch.py world:=/home/user1/ros2_ws/src/gazebo_sfm_plugin/worlds/GAM.world &
+ros2 launch gazebo_ros gazebo.launch.py world:=../worlds/GAM.world &
 sleep 1;
 yarprun --server /console --log &
 sleep 1;

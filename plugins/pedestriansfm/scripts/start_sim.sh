03a06bd6fbe93ea741a80a06adc0f5296c6207a7
morpheus1820 <rosa.stefano@gmail.com>
2023-05-29 11:06:55 +0200

    cleanup and standing anim fix

--- plugins/pedestriansfm/scripts/start_sim.sh
+++ plugins/pedestriansfm/scripts/start_sim.sh
@@ -1,6 +1,6 @@
 yarpserver &
 sleep 1;
-ros2 launch gazebo_ros gazebo.launch.py world:=../worlds/GAM.world &
+ros2 launch gazebo_ros gazebo.launch.py extra_gazebo_args:=-slibgazebo_yarp_clock.so world:=../worlds/GAM.world &
 sleep 1;
 yarprun --server /console --log &
 sleep 1;
